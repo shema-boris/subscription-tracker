2fba0fa61a8e9ba4e625ddb1edcd472f9a214323
Shema Boris Benimana <sheboris123@gmail.com>
2025-05-08 21:04:20 -0400

    Added autn=h controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,48 @@
-export const signUp =async (req, res, next) => {}
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt js'
+import jwt from 'jsonwebtoken';
+export const signUp =async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const {name, email, password} = req.body;
+
+        //Check if a user already exists
+        const existingUser = await User.findOne({email});
+
+        if (existingUser) {
+            const error = new Error('User already exists');
+            error.statusCode=409;
+            throw error;
+        }
+
+        // Hash password
+
+        const salt = await bcrypt.gensalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await user.create([{name, email, password: hashedPassword}], {session});
+
+        const token = jwt.sign({userId: newUsers[0].id }, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN})
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            data: {
+                token,
+                user: newUser[0],
+            }
+        })
+    } catch (error) {
+        await session.abortTransaction();
+         session.endSession();
+        next(error);
+    }
+}
 
 export const signIn = async(req, res, next) => {}
 
