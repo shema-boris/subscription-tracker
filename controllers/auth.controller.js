25fc7f644826d53a91393c30f5f3c83420f27043
Shema Boris Benimana <sheboris123@gmail.com>
2025-05-09 08:54:28 -0400

    Added middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt js'
 import jwt from 'jsonwebtoken';
+
+import User from '../models/user.model.js';
+import {JWT_SECRET, JWT_EXPIRES_IN} from "../config/env.js";
+
 export const signUp =async (req, res, next) => {
     const session = await mongoose.startSession();
     session.startTransaction();
@@ -22,7 +26,7 @@ export const signUp =async (req, res, next) => {
         const salt = await bcrypt.gensalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = await user.create([{name, email, password: hashedPassword}], {session});
+        const newUser = await User.create([{name, email, password: hashedPassword}], {session});
 
         const token = jwt.sign({userId: newUsers[0].id }, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN})
 
@@ -44,6 +48,39 @@ export const signUp =async (req, res, next) => {
     }
 }
 
-export const signIn = async(req, res, next) => {}
-
 export const signOut = async(req, res, next) => {}
+
+export const signIn = async(req, res, next) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if (!user) {
+            const error =new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            const error = new Error('Invalid password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({userId:user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+        res.status(200).json({
+            success: true,
+            message: 'user signed in successfully',
+            data: {
+                token,
+                user,
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
+
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 
+import authorize from '../middlewares/auth.middleware.js'
+import {getUsers, getUser} from '../controllers/user.controller.js'
 const userRouter = Router();
 
-userRouter.get('/', (req,res) => res.send ({title:'GET all users'}));
-userRouter.get('/:id', (req,res) => res.send ({title:'GET user details'}));
+userRouter.get('/', getUsers);
+userRouter.get('/:id', authorize, getUser );
 userRouter.post('/', (req,res) => res.send ({title:'CREATE new user '}));
 userRouter.put('/:id', (req,res) => res.send ({title:'UPDATE user'}));
 userRouter.delete('/', (req,res) => res.send ({title:'DELETE user'}));
