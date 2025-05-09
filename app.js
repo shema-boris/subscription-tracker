f6bc68b101e44a2ea0704c1687b47285cf976598
Shema Boris Benimana <sheboris123@gmail.com>
2025-05-08 20:26:56 -0400

    Connected to database and added authentification

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 import express from 'express';
 import {PORT} from './config/env.js';
-
+import cookieParser from 'cookie-parser'
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+
 app.get('/', (req,res) => {
     res.send('Welcome to the Subscription Tracker API!');
 
 });
 
-app.listen(PORT, () => {
-    console.log('Subscription Tracker API is running on http://localhost:3000');
+app.listen(PORT, async () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+    await connectToDatabase()
 });
 
 export default app;
